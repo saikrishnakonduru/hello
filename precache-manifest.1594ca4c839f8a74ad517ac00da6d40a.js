bc99993e1506945930454a25a3fe03c86c03d92a
saikrishnakonduru <sai.krish14@gmail.com>
2020-06-17 15:34:20 +0530

    Updates

---
+++ precache-manifest.1594ca4c839f8a74ad517ac00da6d40a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "767c094fa084393d3c0a",
+    "url": "/saikrishnakonduru/SaiKrishnaKonduru/static/css/main.c16548db.chunk.css"
+  },
+  {
+    "revision": "767c094fa084393d3c0a",
+    "url": "/saikrishnakonduru/SaiKrishnaKonduru/static/js/main.767c094f.chunk.js"
+  },
+  {
+    "revision": "4749284210335cecd133",
+    "url": "/saikrishnakonduru/SaiKrishnaKonduru/static/js/runtime~main.47492842.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/saikrishnakonduru/SaiKrishnaKonduru/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "668f82c9896740b96600a7a032fd9d28",
+    "url": "/saikrishnakonduru/SaiKrishnaKonduru/index.html"
+  }
+];
